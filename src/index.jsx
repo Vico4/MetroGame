dc6ff69335abfb83f187a09daf693e547a583d0a
Vico4 <victoire.stahl@gmail.com>
2022-03-25 16:09:39 +0100

    suppression page result

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-import Results from './pages/Results'
 import Header from './components/Header'
 import Error from './components/Error'
 import { createGlobalStyle } from 'styled-components'
@@ -24,9 +23,6 @@ ReactDOM.render(
         </Route>
         <Route path="/survey/:questionNumber">
           <Survey />
-        </Route>
-        <Route path="/results">
-          <Results />
         </Route>
         <Route>
           <Error />
